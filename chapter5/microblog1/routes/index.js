9ccc912f7d7fbbc02eaec109f814f02033f1f01c
fjj <fjj_321@126.com>
2015-11-18 18:13:49 +0800

    模板修改

--- chapter5/microblog1/routes/index.js
+++ chapter5/microblog1/routes/index.js
@@ -2,7 +2,8 @@ var express = require('express');
 var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	var  slides=[{"imgUrl":"/images/1.jpg","href":"#1","title":"First Thumbnail label","dscrpt":"dscrpt1"},{"imgUrl":"/images/2.jpg","href":"#2","title":"Second Thumbnail label","dscrpt":"dscrpt2"},{"imgUrl":"/images/3.jpg","href":"#3","title":"Third Thumbnail label","dscrpt":"dscrpt3"}]
+  res.render('index', { title1: 'Express',slides:[{"imgUrl":"/images/1.jpg","href":"#1","title":"First Thumbnail label","dscrpt":"dscrpt1"},{"imgUrl":"/images/2.jpg","href":"#2","title":"Second Thumbnail label","dscrpt":"dscrpt2"},{"imgUrl":"/images/3.jpg","href":"#3","title":"Third Thumbnail label","dscrpt":"dscrpt3"}] });
 });
 
 module.exports = router;
